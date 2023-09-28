f722442455044bb02e034e2ec1cf3f45e22b73eb
mehrshad-kh <mjii@protonmail.com>
2023-09-28 20:34:09 +0330

    refactor

--- src/client.cpp
+++ src/client.cpp
@@ -19,7 +19,10 @@ std::vector<uint8_t> Client::retrieveMySalt(const std::string& username)
 
     Status status = stub_->RetrieveSalt(&context, username_as_mk_string, &salt);
     if (!status.ok()) {
-        throw std::runtime_error(fmt::format("{:d} {:s} {:s}", (int)status.error_code(), status.error_message(), status.error_details()));
+        throw std::runtime_error(
+            fmt::format("{:d} {:s} {:s}", static_cast<int>(status.error_code()), 
+            status.error_message(), 
+            status.error_details()));
     }
 
     return std::vector<uint8_t>(salt.value().begin(), salt.value().end());
